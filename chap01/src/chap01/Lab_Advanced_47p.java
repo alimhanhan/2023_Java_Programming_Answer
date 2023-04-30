99cdde1e2d85af741dcd0cf93cce32bd933130f9
Leona <102565567+alimhanhan@users.noreply.github.com>
2023-04-30 13:14:42 +0900

    Add files via upload

---
+++ chap01/src/chap01/Lab_Advanced_47p.java
@@ -0,0 +1,20 @@
+package chap01;
+
+public class Lab_Advanced_47p {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Han Han Han";
+		String number  = "010-0000-0000";
+		String add = "대한민국 00시 00구";
+		String wor = "SWU";
+		
+		int age = 900;
+		
+		System.out.println(name);
+		System.out.println(number);
+		System.out.println(add);
+		System.out.println(wor);
+		System.out.println(age);
+	}
+}
